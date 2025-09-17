e602856da1163895c814cce2ad1345cda0533abe
ngothithanhh <ngothanhthcstanhung@gmail.com>
2025-09-18 01:01:05 +0700

    Lap04

---
+++ Lap04/Lap03/Models/Categories.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+namespace Lap03.Models
+{
+    public class Categories
+    {
+        public string Title { get; set; }
+
+        public List<Categories> GetCategoriesList()
+        {
+            List<Categories> categories = new List<Categories>()
+            {
+                new Categories(){ Title = "Áo dài"},
+                new Categories(){ Title = "Áo lông"},
+                new Categories(){ Title = "Túi xách"},
+                new Categories(){ Title = "Đồng hồ"},
+                new Categories(){ Title = "Ví da"},
+                new Categories(){ Title = "Thắt lưng da"},
+                new Categories(){ Title = "Tủ lạnh"},
+                new Categories(){ Title = "Tivi"},
+                new Categories(){ Title = "Quạt điện"},
+                new Categories(){ Title = "Lò sưởi"}
+            };
+            return categories;
+        }
+    }
+}
---
+++ Lap04/Lap03/ViewComponent/HotProductsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Lap03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lap03.ViewComponents
+{
+    public class HotProductsViewComponent: ViewComponent
+    {
+        protected Products product = new Products();
+
+        public IViewComponentResult Invoke()
+        {
+            var products = product.GetProductsList();
+            return View(products);
+        }
+    }
+}
