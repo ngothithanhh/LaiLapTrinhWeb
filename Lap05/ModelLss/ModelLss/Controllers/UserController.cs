6e8ccda0c96de5922a97f231f64b875b5420d71b
ngothithanhh <ngothanhthcstanhung@gmail.com>
2025-09-25 19:27:02 +0700

    lap05

---
+++ Lap05/ModelLss/ModelLss/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelLss.Models;
+
+namespace ModelLss.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult user()
+        {
+            var objuser = new User();
+            objuser.Id = 1;
+            objuser.Username = "JohnDoe";
+            objuser.Email = "abcxyz@gmail.com";
+            objuser.PasswordHash = "12345";
+            ViewBag.objuser = objuser;
+            return View();
+        }
+    }
+}
