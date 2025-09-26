f6e93854c137328d2b66ab92d6a52486466fda20
ngothithanhh <ngothanhthcstanhung@gmail.com>
2025-09-26 23:53:33 +0700

    lap06_annotation

---
+++ Lap06/day06Annotation/day06Annotation/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using day06Annotation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace day06Annotation.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserManual()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UserManual(User user)
+        {
+            string password = user.Password;
+            if(password.Length < 7)
+            {
+                ViewBag.Message = "Ngu lon nhap sai do dai mat khau roi, ddmm";
+                return View();
+            }
+            else
+            {
+                return Content("Ban da nhap mk thanh cong!!");
+            }
+        }
+
+        public IActionResult UserAnnotation()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lap06/day06Annotation/day06Annotation/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace day06Annotation.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Ban chua nhap Id")]
+        public long Id { get; set; }
+
+        //[RegularExpression]
+
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Tai khoan toi thieu 5 ki tu, toi da 50 ki tu")]
+        [Required(ErrorMessage = "Ban chua nhap Name")]
+        public string Name { get; set; }
+
+        //[Required(ErrorMessage = "Chua nhap Pass")]
+        public string Password { get; set; }
+        
+        public string ConfirmPassword { get; set; }
+
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
