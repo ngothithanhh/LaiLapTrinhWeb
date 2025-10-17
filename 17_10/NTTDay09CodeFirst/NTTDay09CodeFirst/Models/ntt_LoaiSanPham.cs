cd8fc48114897588e7455464e6fc0002f4a76702
ngothithanhh <ngothanhthcstanhung@gmail.com>
2025-10-17 10:30:16 +0700

    CodeFirst

---
+++ 17_10/NTTDay09CodeFirst/NTTDay09CodeFirst/Models/ntt_LoaiSanPham.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NTTDay09CodeFirst.Models
+{
+    [Table("ntt_LoaiSanPham")]
+    //[index(nameof(nttMaLoai))]
+    public class ntt_LoaiSanPham
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long nttId { get; set; }
+        [Display(Name = "Mã loại")]
+        [StringLength(10)]
+        public string nttMaLoai { get; set; }
+        [Display(Name = "Tên loại sản phẩm")]
+        [StringLength(100)]
+        public string nttTenLoai { get; set; }
+        [Display(Name = "Trạng thái")]
+        public bool nttTrangThai { get; set; }
+
+        public ICollection<ntt_SanPham> nttSanPhams { get; set; }
+    }
+}
---
+++ 17_10/NTTDay09CodeFirst/NTTDay09CodeFirst/Models/ntt_SanPham.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NTTDay09CodeFirst.Models
+{
+    [Table("ntt_SanPham")]
+    public class ntt_SanPham
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long nttId { get; set; }
+        [Display(Name = "Mã sản phẩm")]
+        [StringLength(10)]
+        public string nttMaSP { get; set; }
+        [Display(Name = "Tên sản phẩm")]
+        [StringLength(100)]
+        public string nttTenSP { get; set; }
+        [Display(Name = "Hình ảnh")]
+        [StringLength(200)]
+        public string nttHinhAnh { get; set; }
+        [Display(Name = "Số lượng")]
+        public int nttSoLuong { get; set; }
+        [Display(Name = "Đơn giá")]
+
+        public decimal nttDonGia { get; set; }
+        public long nttLoaiSPId { get; set; }
+
+        public ntt_LoaiSanPham nttLoai_SP { get; set; }
+
+    }
+}
