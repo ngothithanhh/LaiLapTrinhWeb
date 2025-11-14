8d4ded833ac675c034dd34e324ce83dd526632a6
ngothithanhh <ngothanhthcstanhung@gmail.com>
2025-11-14 22:48:03 +0700

    lap04

---
+++ TH0711/Lap04/Lap04/Models/Major.cs
@@ -0,0 +1,14 @@
+﻿namespace Lap04.Models
+{
+    public class Major
+    {
+        public Major()
+        {
+            Learners = new HashSet<Learner>();
+
+        }
+        public int MajorId { get; set; }
+        public string MajorName { get; set; }
+        public virtual ICollection<Learner> Learners { get; set; }
+    }
+}
